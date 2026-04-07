9a687dc8022205aaf982c0f1161e1ecc9ecea15f
agorkaissi <agorka@student.agh.edu.pl>
2026-04-07 08:45:49 +0200

    Add detail to Dashboard

--- ui/src/Dashboard.js
+++ ui/src/Dashboard.js
@@ -6,8 +6,20 @@ const Dashboard = () => {
 
     return (
         <div className="container_main">
+            <div className="dashboard_view">
+                <div className="box1">
+                    <ul>
+                        <li>Agents</li>
+                        <li>Chats</li>
+                        <li>Settings</li>
+                        <li>Logs</li>
+                    </ul>
+                </div>
+                <div className="box2">
+                    Organization structure
 
-
+                </div>
+            </div>
         </div>
     );
 };
