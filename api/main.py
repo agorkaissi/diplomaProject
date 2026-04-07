367b51fba985f8b265c546c474150f35e21d1e3a
agorkaissi <agorka@student.agh.edu.pl>
2026-04-07 14:32:50 +0200

    Add agents.db and model.py

--- api/main.py
+++ api/main.py
@@ -1,11 +1,21 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
+from db import Base, engine
+import models
+from models import Agent
 import logging
 
 logging.basicConfig(
     level=logging.ERROR,
     format="%(levelname)s | %(name)s | %(message)s"
 )
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    Base.metadata.create_all(bind=engine)
+    yield
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/healthcheck")
 def healthcheck():
---
+++ api/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Text
+from db import Base
+
+
+class Agent(Base):
+    __tablename__ = "agents"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), unique=True, nullable=True, index=True)
+    description = Column(Text, nullable=True)
+    domain = Column(String(100), nullable=False)
+    tags = Column(Text, nullable=False)
+    source_type = Column(String(255), nullable=False)
+
+    prompt = Column(Text, nullable=False)
+    active = Column(Boolean, nullable=False, default=True)
+
